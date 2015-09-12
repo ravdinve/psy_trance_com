29aafb2838e54606a9cb624c22c9e601f387b972
ravdinve <ravdinve@yandex.ru>
2015-09-13 02:56:07 +0300

    2015.09.13.02.56

---
+++ 4906ccf2-1095-472d-93c0-70cb6daecb7b/psy_trance_com/Controllers/ScrapeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace psy_trance_com.Controllers
+{
+    [Route("scrape")]
+    public class ScrapeController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            var httpResponseMessage = Request.CreateResponse();
+
+            return httpResponseMessage;
+        }
+    }
+}
