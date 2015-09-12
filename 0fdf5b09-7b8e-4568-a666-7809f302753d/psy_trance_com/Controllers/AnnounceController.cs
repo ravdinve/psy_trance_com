4f8c71dbf7825fe2e542af92afe994b5067bfadf
ravdinve <ravdinve@yandex.ru>
2015-09-13 02:57:09 +0300

    2015.09.13.02.57

---
+++ 0fdf5b09-7b8e-4568-a666-7809f302753d/psy_trance_com/Controllers/AnnounceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace psy_trance_com.Controllers
+{
+    [Route("announce")]
+    public class AnnounceController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            var httpResponseMessage = Request.CreateResponse();
+
+            return httpResponseMessage;
+        }
+    }
+}
