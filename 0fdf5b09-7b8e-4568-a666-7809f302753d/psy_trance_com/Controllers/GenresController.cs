413df0db1c05db9efef6898de280752a85d66086
ravdinve <ravdinve@yandex.ru>
2015-09-14 03:43:58 +0300

    2015.09.14.03.43

--- 0fdf5b09-7b8e-4568-a666-7809f302753d/psy_trance_com/Controllers/GenresController.cs
+++ 0fdf5b09-7b8e-4568-a666-7809f302753d/psy_trance_com/Controllers/GenresController.cs
@@ -1,8 +1,32 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+using psy_trance_com.DataAccessLayer;
+using psy_trance_com.DataAccessLayer.Models;
 
 namespace psy_trance_com.Controllers
 {
     public class GenresController : ApiController
     {
+        [HttpGet]
+        public HttpResponseMessage Index(string genreName = null)
+        {
+            using (var dbContext = new DbContext())
+            {
+                IQueryable<Genre> genres = dbContext.Genres.OrderBy(genre => genre.Name);
+
+                if (genreName != null)
+                {
+                    genres = genres.Where(x => x.Name.Contains(genreName));
+                }
+
+                return Request.CreateResponse(genres.Select(genre => new Models.Genre
+                {
+                    Id = genre.Id,
+                    Name = genre.Name
+                }).ToList());
+            }
+        } 
     }
 }
